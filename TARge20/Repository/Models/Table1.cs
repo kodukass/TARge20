d7fe8b5ae191e999dfa7d1423bc48d03981bc952
Emma :) <99496596+kodukass@users.noreply.github.com>
2022-05-27 13:33:09 +0300

    mingi asi

---
+++ TARge20/Repository/Models/Table1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace TARge20.Repository.Models
+{
+    [Table("Table_1")]
+    public partial class Table1
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(10)]
+        public string FirstName { get; set; }
+        [StringLength(10)]
+        public string LastName { get; set; }
+    }
+}
---
+++ TARge20/Repository/testDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using TARge20.Repository.Models;
+
+#nullable disable
+
+namespace TARge20.Repository
+{
+    public partial class testDbContext : DbContext
+    {
+        public testDbContext()
+        {
+        }
+
+        public testDbContext(DbContextOptions<testDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Table1> Table1s { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Table1>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.FirstName).IsFixedLength(true);
+
+                entity.Property(e => e.LastName).IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
